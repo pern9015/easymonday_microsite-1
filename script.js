a124d990048579a17426d5c5e5dc2a716a72bd52
pern9015 <pern9015@stud.kea.dk>
2021-10-11 12:11:21 +0200

    instagram impedded

--- script.js
+++ script.js
@@ -12,3 +12,53 @@ words.forEach((word) => {
   tl.to(".text", { duration: 1, text: word });
   masterTL.add(tl);
 });
+
+// __________scolltrigger___________\\
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = document.querySelector("section");
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#section2",
+      start: "top 50%",
+      end: "+=200",
+      scrub: true,
+      markers: true,
+    },
+  })
+  .from(".animation1", {
+    duration: 1,
+    opacity: 0,
+  })
+  .from(".animation2", {
+    y: 100,
+    duration: 1,
+    opacity: 0,
+    stagger: 1,
+  })
+  .from(".animation3", {
+    y: 100,
+    duration: 1,
+    opacity: 0,
+    stagger: 1,
+  });
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#section5-5",
+      start: "25% 50%",
+      end: "+=135",
+      scrub: true,
+      markers: true,
+    },
+  })
+  .from(".animation4", {
+    scale: 0,
+    transformOrigin: "100% 100%",
+    duration: 1,
+    opacity: 0,
+    stagger: 1,
+  });

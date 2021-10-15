8e6acdfa026e8284422aadec663ebaf6b1565378
pern9015 <pern9015@stud.kea.dk>
2021-10-15 12:35:24 +0200

    komprimerede billeder og berttest

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const words = [
   "Der giver dig tid til det, der betyder noget.",
   "Du glæder dig til at lave.",
-  "Dine børn ikke bytter væk",
+  "Dine børn ikke bytter væk.",
   "Der giver dig en easy Monday.",
 ];
 
@@ -77,3 +77,34 @@ gsap.utils.toArray(".slideText").forEach((text) => {
     .fromTo(text, { x: 250 }, { x: -250 }, 0)
     .from(text.nextElementSibling, { scale: 0.8 }, 0);
 });
+
+// _________Pil-bouncher________\\
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+  document.querySelector("#arrow").classList.add("bouncing");
+}
+
+//___________tilbage til toppen knap______\\
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
